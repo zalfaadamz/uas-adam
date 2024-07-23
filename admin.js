3ac8d24d0465b2f52f3f0d46815736432bdc604b
zalfaadamz <155212716+zalfaadamz@users.noreply.github.com>
2024-07-23 04:52:24 +0000

    2

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 // middleware/admin.js
 module.exports = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
+  if (req.user && req.user.role === 'user') {
     next();
   } else {
     res.status(403).send('Access denied');
