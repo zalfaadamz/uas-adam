95d9266fb45108bdfaf47d5fffc8f73466d20e3b
zalfaadamz <155212716+zalfaadamz@users.noreply.github.com>
2024-07-23 05:02:51 +0000

    4

--- admin.js
+++ admin.js
@@ -1,8 +1,24 @@
-// middleware/admin.js
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
-  if (req.user && req.user.role === 'user') {
-    next();
-  } else {
-    res.status(403).send('Access denied');
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).send('Access token is missing');
   }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).send('Invalid access token');
+    }
+
+    if (user.role === 'user') {
+      req.user = user;
+      next();
+    } else {
+      res.status(403).send('Access denied');
+    }
+  });
 };
+
